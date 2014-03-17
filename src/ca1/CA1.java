ae94489f47f2494c2ca589b2d507b3f5bb9cff2b
Mark Tobin <mtspark@gmail.com>
2014-03-17 16:52:42 +0000

    Add ParaCalc methods and test class with main method

---
+++ src/ca1/CA1.java
@@ -0,0 +1,86 @@
+package ca1;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * This class generates 50,000 coordinates within the (0,0) (3.1415, 9.8590)
+ * rectangle. An ArrayList is used to store coordinates and iterated over to
+ * find what proportion of coordinates are below the curve y=x^2. ie. The monte
+ * carlo method is used to find the area under that curve in the range x = 0 to
+ * x = pi. CA1 has a static nested class used for creating the ArrayList and
+ * storing.
+ * 
+ * @author Mark Tobin
+ * */
+public class CA1 {
+	/**
+	 * A class which contains the methods to populate an ArrayList, calculate
+	 * the Monte Carlo percentage and the area under the curve.
+	 * 
+	 * @author Mark Tobin
+	 */
+	static class ParaCalc {
+		/** Upper bounds of the rectangle (Lower bounds implicitly 0) */
+		double maxX = 3.1415;
+		/** Upper bounds of the rectangle (Lower bounds implicitly 0) */
+		double maxY = 9.8590;
+		/** Area of the rectangle */
+		double area = maxX * maxY;
+		/** List which will store coordinates */
+		List<Coordinate> coordinateList = new ArrayList<Coordinate>();
+
+		/**
+		 * A method which generates 50000 Coordinates and adds them to a list
+		 */
+		public void createList() {
+			Random r = new Random();
+			for (int i = 0; i < 50000; i++) {
+				// A random between 0 and the upper bound on the x axis
+				double x = maxX * r.nextDouble();
+				// A random between 0 and the upper bound on the y axis
+				double y = maxY * r.nextDouble();
+				// Create a coordinate and add it to the arrayList.
+				coordinateList.add(new Coordinate(x, y));
+			}
+		}
+
+		/**
+		 * A method which calls createList() and finds the percentage of
+		 * coordinates which lie below the curve y=x^2 bounded by x=0, x=pi
+		 * 
+		 * @return percentage - Percentage of points which lie below the curve
+		 */
+		public double findPercentage() {
+			createList();
+			// Create an iterator to iterate over the arrayList
+			Iterator<Coordinate> it = coordinateList.iterator();
+			double count = 0;
+			while (it.hasNext()) {
+				Coordinate c = it.next(); // An x, y coordinate
+				if (c.y < (c.x * c.x)) { // If the coordinate lies below the
+											// curve
+					count++; // Count values below curve
+				}
+			}
+			// Calulate the percentage of points below the curve.
+			double percentage = (count / 50000) * 100;
+			return percentage;
+		}
+
+		/**
+		 * Method calls findPercentage() and uses the Monte Carlo percentage to
+		 * calculate area.
+		 * 
+		 * @return areaCalc - The calculate area under the curve y=x^2 bounded
+		 *         by 0 and pi.
+		 */
+		public double findArea() {
+			double percentage = findPercentage();// Percentage of coordinates
+													// below the curve.
+			double areaCalc = (area * percentage) / 100; // Area under curve.
+			return areaCalc;
+		}
+	}
+}
---
+++ src/ca1/CA1Test.java
@@ -0,0 +1,41 @@
+package ca1;
+import ca1.CA1.ParaCalc;
+
+/**
+ * Class which prints the value of the area under a curve y=x^2 from 0 to pi.
+ * Contains a main method which calls the methods of CA1.java and prints the
+ * results. Tests the calculated value against the value solved by analysis of
+ * the integral.
+ * 
+ * @author Mark Tobin
+ * 
+ */
+public class CA1Test {
+	/**
+	 * Calculate the value of INT(x^2)dx|x=0 to x=pi
+	 * 
+	 * @return area - The area under the curve y=x^2 bounded by x=0,3.1415
+	 */
+	public static double valueByIntegration() {
+		double area = (3.1415 * 3.1415 * 3.1415) / 3;
+		return area;
+	}
+
+	/**
+	 * Method which calls the methods of CA1 and prints the resulting area. Also
+	 * prints the solved area.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CA1.ParaCalc pc = new CA1.ParaCalc();
+		System.out
+				.println("The area under the curve y=x^2 bounded by x=0 and x=pi");
+		System.out.print("Estimated using the Monte Carlo method:	");
+		System.out.println(pc.findArea());
+		System.out.print("Solved by analysis of INT(x^2)dx|x=0 to x=pi:	");
+		System.out.println(valueByIntegration());
+
+	}
+
+}
