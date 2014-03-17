308c5c489c8a2fdc5e94bb87752721172106d0cf
Mark Tobin <mtspark@gmail.com>
2014-03-17 13:15:27 +0000

    Add code for coordinate class

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,4 +1,15 @@
-
-public class Coordinate {
-
-}
+/** A class which represents a coordinate on the x, y plane. */
+public class Coordinate {         
+    public double x; 
+    public double y; 
+    /** Class constructor which takes a pair of double values to 
+     * create a coordinate point. */
+    public Coordinate(double x, double y) {         
+        this.x = x; 
+        this.y = y; 
+     }
+    /** A method which returns a coordinate as a string */
+    public String stringCoordinate(){
+    	return "(" + Double.toString(x) + ", " + Double.toString(y) + ")"; 
+    }
+ }
